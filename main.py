9d30cd34bde1f1f6a993644cede9e271a0abf8e6
Andrew Francis <andrewmfrancis@icloud.com>
2020-11-30 15:28:56 -0700

    Hotel Rates Completed

--- main.py
+++ main.py
@@ -24,14 +24,45 @@ hotel_rates = {
 
 HOTEL_BUDGET = 850
 
-# ...
+# finding the route
 
-# ...
+
+def find_route():
+    """ This will take the highest average of all the permutations of the cities'
+    temperatures to find a route"""
+    # test_perm = []
+    # cities, temps = zip(*city_temps)
+    # for i in temps:
+    #     for t in i:
+    #         test_perms.append(permutations())
+
+
+# finding the hotel
+days = len(city_temps)
+
+
+def find_max():
+    """ This will give us the best combination for which hotels
+    to stay to use as much of our budget as possible."""
+    test_hotels = list(filter(lambda x: sum(x) <= HOTEL_BUDGET,
+                              (combinations_with_replacement(hotel_rates.values(), days))))
+    return max(test_hotels)
+
+
+def list_hotel(lst):
+    """This returns the names of the hotels for the best fitting our budget"""
+    hotels = []
+    for i in lst:
+        for key, value in hotel_rates.items():
+            if i == value:
+                hotels.append(key)
+    return hotels
+
 
 if __name__ == "__main__":
     cities = list(city_temps.keys())
-    hotels = list(hotel_rates.keys())
     # ..
     print(f'Here is your best route: {None} the average of the daily max temp. is {None}F')
-    # ..
-    print(f'To max out your hotel budget, stay at these hotels: {None}, totaling ${None}')
+
+    print(f'To max out your hotel budget, stay at these hotels:'
+          f' {", ".join((list_hotel(find_max())))},\ntotaling ${sum(find_max())}')
